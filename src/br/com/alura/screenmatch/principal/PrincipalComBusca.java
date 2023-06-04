0917e71128335f5fded1b81e464656a070652344
Danillo <danillogoncalves001@gmail.com>
2023-06-04 19:52:21 -0300

    Transformando dados

--- src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
+++ src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
@@ -1,4 +1,27 @@
 package br.com.alura.screenmatch.principal;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Scanner;
+
 public class PrincipalComBusca {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        Scanner leitura = new Scanner(System.in);
+        System.out.print("Digite um filme para busca: ");
+        String busca = leitura.next();
+
+        String endereco = "https://www.omdbapi.com/?t=" + busca + "&apikey=15d3e205";
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(endereco))
+                .build();
+        HttpResponse<String> response = client
+                .send(request, HttpResponse.BodyHandlers.ofString());
+        System.out.println(response.body());
+        System.out.println(response.statusCode());
+    }
 }
